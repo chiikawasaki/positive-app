08c3f2aeec7dec59b4a69d9138b7ed4204796462
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-04-25 20:47:00 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+const config = {
+  theme: {
+    extend: {
+      colors: {
+        cream: "#fef6e4", // ← ここで色に名前をつけて登録
+      },
+    },
+  },
+};
+
+export default config;
